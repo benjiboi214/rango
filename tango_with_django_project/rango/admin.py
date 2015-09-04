c9f4e1c752860b7764460ea9853d146769dd3ead
Ben <benjiboi214@gmail.com>
2015-09-04 16:04:37 +1000

    Finished the last of the second django tutorial.
    This involved modifying the admin interface, changing how each class is displayed in the admin window, and how it can be searched.
    
    Lastly, replaced a default template with one that renamed the admin header to "Rango Project"

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,         {'fields': ['name']}),
+        ('Popularity', {'fields': ['likes', 'views'], 'classes': ['collapse']}),
+    ]
+    list_display = ('name', 'likes', 'views')
+    #list_filter = ['likes']    #Adds filter to category admin page.
+    search_fields = ['name']    #Adds search bar for name field.
+
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Page Information', {'fields': ['title', 'category', 'url']}),
+        ('Popularity',       {'fields': ['views'], 'classes': ['collapse']}),
+    ]
+    list_display = ('title', 'category', 'views')
+    search_fields = ['title', 'url']
+        
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
