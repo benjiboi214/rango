6a095596795041924224faa20d4754e65ee4ea8d
Ben <benjiboi214@gmail.com>
2015-09-18 17:25:33 +1000

    Finished Exercise 7

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,8 +3,14 @@ from rango import views
 
 urlpatterns = patterns('',
     
+    #see the following if the category regex is confusing.
+    #the way it passes the slug to the following function.
+    #https://docs.djangoproject.com/en/1.8/topics/http/urls/#views-extra-options
+    #https://docs.djangoproject.com/en/1.8/ref/urls/
+    
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     url(r'^benweb/', views.benweb, name='benweb')
 
 )
