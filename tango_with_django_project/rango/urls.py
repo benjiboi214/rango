47274372f4778f449147dd36ceb43852ffab5b53
Ben Elliot <benjiboi214@gmail.com>
2015-10-15 15:28:05 +1100

    Finshed Chapter 12, installed simple registration on my website. -moved the old urls entries to the bottom of urls.py -added the password change templates -moved the base.html into the rango folder. -modified the templates so theyr referred to the correct path to base.html.

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -14,9 +14,9 @@ urlpatterns = patterns('',
     url(r'^benweb/$', views.benweb, name='benweb'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
-    #url(r'^register/$', views.register, name="register"),
-    #url(r'^login/$', views.user_login, name="login"),
     url(r'^restricted/$', views.restricted, name="restricted"),
     #url(r'^logout/$', views.user_logout, name="logout"),
+    #url(r'^register/$', views.register, name="register"),
+    #url(r'^login/$', views.user_login, name="login"),
 )
 #Comment out original Login, Logut and Register function for review later.
