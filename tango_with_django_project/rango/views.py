a86c7503c950af4980f5eb5d5db9e4206d0d0adc
Ben Elliot <benjiboi214@gmail.com>
2015-10-30 14:59:25 +1100

    Cleaned up the old forms for old registration.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from rango.models import Category, Page
-from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from rango.forms import CategoryForm, PageForm#, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
